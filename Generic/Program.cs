cc4b734bb071645b1610927f85b69d30db9255d7
pavani <pavaniavni456@gmail.com>
2022-02-07 10:40:07 +0530

    added generic

---
+++ Generic/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Generic
+{
+    internal class Program
+    {
+        public class sample<P>
+        {
+            //field
+            private P data;
+
+            //property
+            public P value
+            {
+                //accessors
+                get
+                {
+                    return this.data;
+                }
+                set
+                {
+                    this.data = value;
+                }
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            sample<string> name = new sample<string>();
+            name.value = "Pavani";
+
+            sample<float> version = new sample<float>();
+            version.value = 6.0F;
+
+            sample<int> count = new sample<int>();
+            count.value = 84;
+            //display Name
+            Console.WriteLine(name.value);
+            //display version
+            Console.WriteLine(version.value);
+            //display count
+            Console.WriteLine(count.value);
+        }
+    }
+}
